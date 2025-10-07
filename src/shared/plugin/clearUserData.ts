9ca40f672e6797d7b282c563a0e0bba4ed1940a4
mbpxl <esscof1e@gmail.com>
2025-10-08 01:25:35 +0300

    fix(app) fixed #1-#4 points from list

--- src/shared/plugin/clearUserData.ts
+++ src/shared/plugin/clearUserData.ts
@@ -1,8 +1,15 @@
 export function clearUserData() {
   localStorage.removeItem("access_token");
   localStorage.removeItem("refresh_token");
+  localStorage.removeItem("captcha_token");
   localStorage.removeItem("signin-form");
   localStorage.removeItem("signup-form");
   localStorage.removeItem("search_filter");
   localStorage.removeItem("about");
+}
+
+//* функция для очистки данных с прошлой сессии при входе, иначе ошибка получения access_token
+export function clearAuthUserData() {
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("refresh_token");
 }
